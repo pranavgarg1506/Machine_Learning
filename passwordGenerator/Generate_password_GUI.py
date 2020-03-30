26caea9f7e02b13d5d018d2de2c40390c676ec78
pranav garg <pranavfunny1@gmail.com>
2020-03-30 17:24:55 +0530

    probability_weightage_added

--- passwordGenerator/Generate_password_GUI.py
+++ passwordGenerator/Generate_password_GUI.py
@@ -1,23 +1,23 @@
-import numpy as np
 import datetime
 from tkinter import *
 from tkinter.ttk import *
-import pyperclip
 
 Capital = "ABCDEFGIJKLMNOPQRSTUVWXYZ"
 small = 'abcdefgijklmnopqrstuvwxyz'
 number = '0123456789'
 specialCharacters = '!@#$%^&*()'
 
-
 # digit = int(input())
 # print("digit ", digit)
 
 # defining probabilities for all the sets
-# p_Capital = 0
-# p_small = 0
-# p_number = 0
-# p_character = 0
+p_Capital = 0
+p_small = 0
+p_number = 0
+p_character = 0
+
+# initializing probability array for further calculation
+p_array = [p_Capital, p_small, p_number, p_character]
 
 
 # calculating the currentSum on the basis of current Time
@@ -31,10 +31,11 @@ def choice1():
     currentSecond = currentTimeStamp.second
     currentMicroSecond = currentTimeStamp.microsecond
     currentSum = currentYear + currentMonth + currentDay + currentHour + currentMinute + currentSecond + currentMicroSecond
-    print("currentSum ", currentSum)
+    # print("currentSum ", currentSum)
     return currentSum
 
 
+#  flag is passed for the choice (0 for Capital, 1 for small, 2 for numbers, 3 for Special Characters)
 def checkForDuplicates(currentSum, flag, password):
     byMod = 0
     if flag == 0:
@@ -64,52 +65,107 @@ def checkForDuplicates(currentSum, flag, password):
 def choiceCapital(currentSum, password):
     # index = currentSum % 26
     index = checkForDuplicates(currentSum, 0, password)
-    print("value of index for Capital", index)
-    print("Capital index ", Capital[index])
+    # print("value of index for Capital", index)
+    # print("Capital index ", Capital[index])
     return Capital[index]
 
 
 def choiceSmall(currentSum, password):
     # index = currentSum % 26
     index = checkForDuplicates(currentSum, 1, password)
-    print("value of index for Small", index)
-    print("small index", small[index])
+    # print("value of index for Small", index)
+    # print("small index", small[index])
     return small[index]
 
 
 def choiceNumber(currentSum, password):
     # index = currentSum % 10
     index_number = checkForDuplicates(currentSum, 2, password)
-    print("value of index for number", index_number)
-    print("number index", number[index_number])
+    # print("value of index for number", index_number)
+    # print("number index", number[index_number])
     return number[index_number]
 
 
 def choiceCharacters(currentSum, password):
     # index = currentSum % 10
     index_character = checkForDuplicates(currentSum, 3, password)
-    print("value of index for characters", index_character)
-    print("character index", specialCharacters[index_character])
+    # print("value of index for characters", index_character)
+    # print("character index", specialCharacters[index_character])
     return specialCharacters[index_character]
 
 
+# high chances of optimization
+def processingProbabilityArray(p_array):
+    temp_array = []
+    # code for processing
+
+    # first case :- check if all the elements are 1
+    '''
+    flag_for_one = True
+    for i in p_array:
+        if i != 1:
+            flag_for_one = False
+    if flag_for_one:
+        return [0,1,2,3]
+    '''
+
+    # second case:- checking if all the elements are integer
+    flag_for_Integer = True
+    for i in p_array:
+        if i != int(i):
+            flag_for_Integer = False
+    if flag_for_Integer:
+        for i in range(0, len(p_array)):
+            while p_array[i] > 0:
+                temp_array.append(i)
+        return temp_array
+
+    # third case:- if the elements are of float type
+    p_array_tempobj = p_array.copy()
+
+    p_array_tempobj = [i * 10 for i in p_array_tempobj]
+
+    for i in range(0, len(p_array_tempobj)):
+        while p_array_tempobj[i] > 0:
+            temp_array.append(i)
+    return temp_array
+
+
+def calculateElementForPassword(p_array, currentSum):
+    temp_array = processingProbabilityArray(p_array)
+    temp_array_length = len(temp_array)
+    index_temp = currentSum % temp_array_length
+    return temp_array[index_temp]
+
+
+def calculateProbabilityArray(p_arrayobj, indexForChoosingClass):
+    for i in range(0, len(p_arrayobj)):
+        if i != indexForChoosingClass:
+            p_arrayobj[i] = p_arrayobj[i] + 0.2
+    return p_arrayobj
+
+
 def main():
+    # setting the probability array as 1 for all the values for equal probability selection
+    p_array = [1, 1, 1, 1]
     # setting the password field blank for every instance of password generation
     var.set("")
     password = ''
     digit = var1.get()
     for index in range(0, digit):
         currentSum = choice1()
-        if currentSum % 4 == 0:
+        indexForChoosingClass = calculateElementForPassword(p_array, currentSum)
+        p_array = calculateProbabilityArray(p_array, indexForChoosingClass)
+        if indexForChoosingClass == 0:
             character = choiceCapital(currentSum, password)
             password = password + character
-        elif currentSum % 4 == 1:
+        elif indexForChoosingClass == 1:
             character = choiceSmall(currentSum, password)
             password = password + character
-        elif currentSum % 4 == 2:
+        elif indexForChoosingClass == 2:
             character = choiceNumber(currentSum, password)
             password = password + character
-        elif currentSum % 4 == 3:
+        elif indexForChoosingClass == 3:
             character = choiceCharacters(currentSum, password)
             password = password + character
         else:
@@ -120,8 +176,8 @@ def main():
 
 
 def Reset():
-    var.set("")
-    var1.set("")
+    var.set("")  # used for password field
+    var1.set("")  # usd for length field
 
 
 def Copy():
@@ -135,6 +191,9 @@ root = Tk()
 var = StringVar()
 var1 = IntVar()
 
+height = root.winfo_screenheight()
+print("height", height)
+
 # Title of your GUI window
 root.title("Random Password Generator")
 
@@ -142,7 +201,7 @@ root.title("Random Password Generator")
 # Label 1
 Random_password = Label(root, text='Password')
 Random_password.grid(row=0)
-entry = Entry(root, textvariable = var)
+entry = Entry(root, textvariable=var)
 entry.grid(row=0, column=1)
 
 # label 2
